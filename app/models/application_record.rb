64ae78071cc0a1e050127c562d29c3760a08080a
leonatoieiri <leonatoieiri@gmail.com>
2022-04-03 23:32:47 -0300

    Added uuid and roles to user

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  before_create :set_uuid
+  
+  def set_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+    enum :role, [:reader, :publisher, :admin]
 end
